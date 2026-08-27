d424be81d483723beecbd608a43bfbd3aa2460bc
Pigletnaja <loukmoo12@gmail.com>
2026-08-27 22:26:30 +0700

    Finish

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject adjustPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StartNewGame()
+    {
+        Settings.fromSave = true;
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void LoadSaveGame()
+    {
+        Settings.fromSave = false;
+        SceneManager.LoadScene("Loading");
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void ShowHideAdjustPanel(bool flag)
+    {
+        adjustPanel.SetActive(flag);
+    }
+}
